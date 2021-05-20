1b2d89ab65c7fb056bdf28cd372703ee3848125d
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-05-19 21:32:25 -0400

    getServerSideProps and getStaticProps

---
+++ config/index.js
@@ -0,0 +1,3 @@
+export const API_URL =
+  // 'next public' visible within client (Nextjs environment varialbes requires this)
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     // wrapping content in layout instead of div
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+// can be above or below component
+// 'server side' runs every time go to page
+// 'static' makes request at build time and then will build out static pages
+// if someone updates event, won't see because it ran at build time (revalidated)
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1, // only if data has changed
+  }
 }
