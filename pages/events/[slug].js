915d773997bc36908577358cc539d92088d8e928
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-06-01 21:56:07 -0400

    single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,54 @@
-// brackets for dynamic routing ([] and whatever params you want)
 // this file for /events/slug
+// brackets for dynamic routing ([] and whatever params you want)
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
 
 export default function EventPage({ evt }) {
+  const deleteEvent = e => {
+    console.log('delete')
+  }
+
   console.log(evt)
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt />
+              Edit Event
+            </a>
+          </Link>
+          {/* going nowhere because it's not a link, it's a method */}
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes />
+            Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   )
 }
